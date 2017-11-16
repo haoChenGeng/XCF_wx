93476913f07ce9447002130a9ed6b36b7a31ac70
cjw <chenjiawei4@xiaoniu66.com>
2017-11-16 11:03:12 +0800

    2017-11-16

--- application/controllers/jijin/FixedInvestmentController.php
+++ application/controllers/jijin/FixedInvestmentController.php
@@ -60,19 +60,20 @@ class FixedInvestmentController extends MY_Controller
 					$fundinfo['fundname'] = $fundInfo['fundname'];
 					$fundinfo['per_min_39'] = $fundInfo['per_min_39'];
 					$fundinfo['per_max_39'] = $fundInfo['per_max_39'];
-					if((int)$_SESSION['riskLevel'] < (int)$fundinfo['risklevel'])
+					$data['token'] = $_SESSION['token'] = mt_rand(100000,999999);
+					if((int)$_SESSION['riskLevel'] < (int)$fundinfo['risklevel']){
 						$data['riskmatching'] = 0;
+						if((int)$_SESSION['riskLevel'] == 1){
+							$data['riskmatching'] = 2;
+							$data['token'] = 0;
+						} 
+					}
 					else
 						$data['riskmatching'] = 1;
 					$this->load->config('jz_dict');
 					$fundinfo['risklevel'] =isset($this->config->item('productrisk')[$fundinfo['risklevel']])?$this->config->item('productrisk')[$fundinfo['risklevel']]:null;
 					$data['my_risklevel'] =isset($this->config->item('custrisk')[$_SESSION['riskLevel']])?$this->config->item('custrisk')[$_SESSION['riskLevel']]:null;
-					$data['token'] = $_SESSION['token'] = mt_rand(100000,999999);
 					$data['public_key'] = file_get_contents($this->config->item('RSA_publickey'));
-					if((int)$_SESSION['riskLevel'] == 1){
-						$data['riskmatching'] = 2;
-						$data['token'] = 0;
-					}
 					$return['code'] = 0;
 					$return['data'] = $data;
 				}else{
@@ -169,7 +170,7 @@ class FixedInvestmentController extends MY_Controller
 			foreach ($fixed['data'] as $key => $value) {
 				$fixed['data'][$key]['channelname'] = $channel_info[$value['channelid']]['channelname'];
 			}
-			
+			$fixed['data']['public_key'] = file_get_contents($this->config->item('RSA_publickey'));
 			$return['code'] = 0;
 			$return['msg'] = $fixed['msg'];
 			$return['data'] = $fixed['data'];
